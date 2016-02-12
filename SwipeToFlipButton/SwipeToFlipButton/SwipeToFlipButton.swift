8fb802cd0371aae1f1966c7a853f9c6dfea0283d
Jason Lo <jason@losoftware.co.uk>
2016-02-12 16:04:46 +0000

    initial commit 2

---
+++ SwipeToFlipButton/SwipeToFlipButton/SwipeToFlipButton.swift
@@ -0,0 +1,128 @@
+//
+//  SwipeToFlipButton.swift
+//  Flex test
+//
+//  Created by Jason Lo on 11/02/2016.
+//  Copyright © 2016 thehut. All rights reserved.
+//
+
+import UIKit
+
+public class SwipeToFlipButton: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var seqArray: NSArray   //array of strings or values for display on label
+    var currentItem: Int
+    
+    var backShowing = false
+    var backView: UILabel
+    var frontView: UILabel
+    
+    var alignment: NSTextAlignment = NSTextAlignment.Center
+    var animateDuration: Double = 0.375
+    var buttonFont: UIFont = UIFont(name: "Helvetica", size: 14)!
+    
+    public init(frame: CGRect, sequenceArray: NSArray, defaultArrayItem: Int) {
+        
+        seqArray = sequenceArray
+        currentItem = defaultArrayItem
+        
+        backView = UILabel(frame: CGRect(origin: CGPoint(x: 0,y: 0), size: frame.size))
+        backView.backgroundColor = UIColor.whiteColor()
+        backView.textAlignment = alignment
+        backView.font = buttonFont
+        frontView = UILabel(frame: CGRect(origin: CGPoint(x: 0,y: 0), size: frame.size))
+        frontView.backgroundColor = UIColor.whiteColor()
+        frontView.text = "\(seqArray[currentItem])"
+        frontView.textAlignment = alignment
+        frontView.font = buttonFont
+        
+        super.init(frame: frame)
+        
+        let rightSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        rightSwipeGesture.direction = UISwipeGestureRecognizerDirection.Right
+        let leftSwipeGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        leftSwipeGesture.direction = UISwipeGestureRecognizerDirection.Left
+        
+        self.addGestureRecognizer(rightSwipeGesture)
+        self.addGestureRecognizer(leftSwipeGesture)
+        self.userInteractionEnabled = true
+        
+        self.addSubview(frontView)
+    }
+
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    func swiped(sender: AnyObject) {
+        if sender.isKindOfClass(UISwipeGestureRecognizer) {
+            
+                let swipeGesture: UISwipeGestureRecognizer = sender as! UISwipeGestureRecognizer
+                
+                var showString: String = "\(seqArray[currentItem])"
+                print(currentItem)
+                
+                switch  swipeGesture.direction {
+                case UISwipeGestureRecognizerDirection.Left:
+                    print("Swipe left")
+                    if currentItem > 0 {
+                        showString = "\(seqArray[--currentItem])"
+                        self.flipView(UIViewAnimationOptions.TransitionFlipFromRight, textString: showString)
+                    }
+                    break
+                case UISwipeGestureRecognizerDirection.Right:
+                    print("Swipe right")
+                    if currentItem + 1 < seqArray.count {
+                        showString = "\(seqArray[++currentItem])"
+                        self.flipView(UIViewAnimationOptions.TransitionFlipFromLeft, textString: showString)
+                    }
+                    break
+                default:
+                    break
+                    
+                }
+            
+            
+            
+            
+            
+        }
+    }
+    
+    func flipView(direction: UIViewAnimationOptions, textString: String) {
+        UIView.transitionFromView(backShowing ? backView : frontView, toView: backShowing ? frontView : backView, duration: animateDuration, options: direction, completion: nil)
+        if backShowing { frontView.text = textString } else { backView.text = textString }
+        backShowing = !backShowing
+    }
+    
+//    func flipLeft() {
+//        if backShowing {
+//            UIView.transitionFromView(backView, toView: frontView, duration: 0.375, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+//        } else {
+//            UIView.transitionFromView(frontView, toView: backView, duration: 0.375, options: UIViewAnimationOptions.TransitionFlipFromLeft, completion: nil)
+//        }
+//        backShowing = !backShowing
+//    }
+//    
+//    func flipRight() {
+//        
+//        if backShowing {
+//            UIView.transitionFromView(backView, toView: frontView, duration: 0.375, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+//        } else {
+//            UIView.transitionFromView(frontView, toView: backView, duration: 0.375, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+//        }
+//        backShowing = !backShowing
+//    }
+    
+
+}
