a4f04b2485baa754332cf00d8b362e11245c5899
Jason Lo <jason@losoftware.co.uk>
2016-02-15 12:15:54 +0000

    Changed init Added setup call to init after setting values

--- SwipeToFlipButton/SwipeToFlipButton/ViewController.swift
+++ SwipeToFlipButton/SwipeToFlipButton/ViewController.swift
@@ -8,16 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SwipeToFlipProtocol {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let topButton: SwipeToFlipButton = SwipeToFlipButton(frame: CGRect(x: 30, y: 40, width: 40, height: 40), sequenceArray: [1,2,3,4,5], defaultArrayItem: 2)
+        //        topButton = SwipeToFlipButton(frame: CGRect(x: 10, y: 40, width: 40, height: 40))
+        topButton.animateDuration = 0.25
+        
+        self.view.addSubview(topButton)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func swipeButtonTapped() {
+        print("swipeButtonTapped")
     }
 
 
